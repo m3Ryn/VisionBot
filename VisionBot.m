56ca5db066496a7c4df20e87d68068502751c5a6
Merin <dydflyer@gmail.com>
2010-09-28 19:09:25 +0530

    Now working with webcam but lagging a lot. Also there are noise problems so not detecting efficiently.

--- VisionBot.m
+++ VisionBot.m
@@ -1,55 +1,62 @@
-function red_recognition
-clear all, close all, clc;
-coord_x_row = [];
-coord_y_row = [];
+clc,clear all,close all;
+
+%Calling the Video
+%---------------------------------------------------------
 
 vid = videoinput('winvideo',1','RGB24_640x480');
+triggerconfig(vid,'manual');
+set(vid,'FramesPerTrigger',1);
+set(vid,'TriggerRepeat',Inf);
 start(vid);
-image = getdata(vid,1);
 
-%image = imread('image.bmp');
-[indexed_image,map] = rgb2ind(image,5); %convert into index image mode
-[m,n] = size(map); % extracting rows ans colums from the map
+while(1)
+    
+    trigger(vid);
+    image = getdata(vid,1);
+    
+    %Passing through a Mean filter to filter out noise
+    %------------------------------------------------------ 
 
-% loop to check if the image contains RED color. 
-% ------------------------------------------------
-for i = 1:m                     
-    if (map(i,:)==[1 0 0]);    
-        j = 1;
-        break;
-    end
-j = 0;
+    H = fspecial('average',[5 5]);
+    rgb = imfilter(image,H);
+
+    rgb = double(rgb)/255;
+    r = rgb(:,:,1);
+    g = rgb(:,:,2);
+    b = rgb(:,:,3);
+
+    %Filter RED Color
+    %------------------------------------------------------
+
+    Rc = r./sqrt(r.^2+g.^2+b.^2)>0.87;
+
+    rgb2(:,:,1) = Rc.*r;
+    rgb2(:,:,2) = Rc.*g;
+    rgb2(:,:,3) = Rc.*b;
+
+
+    rgb2_filtered = imfilter(rgb2,H);
+
+
+    indexed_image = rgb2ind(rgb2_filtered,5);
+    %converted into binary image through 'region of interest'
+    roi = roicolor(indexed_image,2);
+    
+    % Coordinates calculation
+    % -------------------------------------------------
+    boundary = bwboundaries(roi); % tracing external boundary
+    coordinates = cell2mat(boundary); %converting into matrix form
+    Y = coordinates(:,1);
+    X = coordinates(:,2);
+
+
+    % show result
+    %clf;subplot(2,2,1);
+    imshow(rgb);
+    rectangle('Position',[min(X),min(Y),(max(X)-min(X)),(max(Y)-min(Y))],'LineWidth',2,'EdgeColor','y');    
+
+    %subplot(2,2,2);
+    %imshow(rgb2);
+    %axis image;
 end
-% ------------------------------------------------
-
-% loop to generate a new map matrix with all zeros
-%-------------------------------------------------
-if j == 1
-    for row = 1:m
-        for column = 1:n
-            map_new(row,column) = 0;
-        end
-    end
-end
-% ------------------------------------------------
-map_new(i,:) = [1 0 0]; % making the red index to be filled with red
-
-roi = roicolor(indexed_image,i-1); %converted into binary image through 'region of interest'
-
-% Coordinates calculation
-% -------------------------------------------------
-boundary = bwboundaries(roi); % tracing external boundary
-coordinates = cell2mat(boundary); %converting into matrix form
-Y = coordinates(:,1);
-X = coordinates(:,2);
-
-centre_x = round((max(X)+ min(X))/2);
-centre_y = round((max(Y)+ min(Y))/2);
-% --------------------------------------------------
-subplot(211),imshow(image); % showing the original image
-rectangle('Position',[min(X),min(Y),(max(X)-min(X)),(max(Y)-min(Y))],'LineWidth',2,'EdgeColor','y');
 stop(vid),delete(vid),clear vid;
-%subplot(212),imshow(indexed_image,map_new); % showing the recognized image
-%subplot(313),imshow(roi); % showing region of interest logically
-
- 
